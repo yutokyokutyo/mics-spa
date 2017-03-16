41ca1d08d4904f7a6a3ccd55cc782f8089d7ca1f
y.kondo <y.kondo@pepabo.com>
2017-03-16 10:03:55 +0900

    smartyを用いてPHPとHTMLを分割する

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/smarty/smarty/libs/Smarty.class.php';
+
+$smarty = new Smarty();
+$smarty->template_dir = 'templates/';
+$smarty->compile_dir  = 'templates_c/';
+
+$smarty->display('index.html');
